bf30740171d0214bec5752d81b4a23811edcc9bc
Douglas Borges <douglasborges19@gmail.com>
2022-12-27 12:53:33 -0300

    try-catch

---
+++ exception-00/Course/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divisão por zero não é permitida.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("A string de entrada não estava no formato correto.");
+            }
+        }
+    }
+}
