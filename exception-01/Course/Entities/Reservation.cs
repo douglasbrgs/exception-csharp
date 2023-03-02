e07bc5918197498d6358d2d3f710b8b495b06a76
Douglas Borges <douglasborges19@gmail.com>
2023-03-02 16:36:13 -0300

    custom exceptions part 1

---
+++ exception-01/Course/Entities/Reservation.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Course.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; private set; }
+        public DateTime CheckOut { get; private set; }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                   + RoomNumber
+                   + ", check-in: "
+                   + CheckIn.ToShortDateString()
+                   + ", check-out: "
+                   + CheckOut.ToShortDateString()
+                   + ", "
+                   + Duration()
+                   + " nights";
+        }
+    }
+}
